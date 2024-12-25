413566edc8e2723850be2d852f7519754f537385
Lexi <teknei@proton.me>
2024-12-25 19:11:26 +0200

    config files

--- nvidia.nix
+++ nvidia.nix
@@ -9,11 +9,12 @@
   # boot.kernelModules = [ "amdgpu" ];
   # boot.initrd.kernelModules = [ "amdgpu" "nvidia" ];
   
-  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-  
-  boot.initrd.kernelModules = [ "nvidia" ];
 
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  
+  # boot.initrd.kernelModules = [ "nvidia" ];
+
+  # boot.kernelParams = [ "nvidia_drm.fbdev=1" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
@@ -30,6 +31,6 @@
 
     forceFullCompositionPipeline = false;
 
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 }
