5c23366f76a3a51faadf9715a59c1eb3d63c795d
asm <git@asm.vefja.org>
2025-04-19 00:38:08 +0300

    update config

--- noexec.nix
+++ noexec.nix
@@ -2,7 +2,5 @@
 
 {
   fileSystems."/".options = [ "noexec" ];
-  fileSystems."/etc/nixos".options = [ "noexec" ];
-  fileSystems."/srv".options = [ "noexec" ];
-  fileSystems."/var/log".options = [ "noexec" ];
+  fileSystems."/var".options = [ "noexec" ];
 }
