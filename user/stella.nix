cc7f9e7cbaebfc355e313eb982eda6544ece99d4
Ada <git@asmx2.dev>
2025-08-24 23:37:48 +0300

    update config (2B)

--- user/stella.nix
+++ user/stella.nix
@@ -4,7 +4,6 @@
   imports = 
     [
       ./wallpaper.nix
-      ./zen-extensions.nix
     ];
 
   home.username = "stella";
@@ -138,6 +137,34 @@
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
+      };
+      DisableFirefoxAccounts = true;
+
+      ExtensionSettings = {
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+        };
+	"{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"addon@fastforward.team" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/fastforwardteam/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"addon@karakeep.app" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/karakeep/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+	"uBlock0@raymondhill.net" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+	  installation_mode = "force_installed";
+	};
       };
     };
   };
